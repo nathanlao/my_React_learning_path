781334874067903bfd3ca4ed429c87156af6645f
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-28 20:33:55 -0800

    Updated footer

--- my-react-learning-app/src/components/Footer.js
+++ my-react-learning-app/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
     return (
         <footer className="footer">
-            <small>@ 2022 Lao developement. All rights reserved.</small>
+            <small>@ 2022 Nathan developement. All rights reserved.</small>
         </footer>
     )
 }
