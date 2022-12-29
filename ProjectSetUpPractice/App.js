1e3fd0d7d8043b43ffbc3953c564da1d5ad2cfc4
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-28 19:03:00 -0800

    Project set up practice

---
+++ ProjectSetUpPractice/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Main from "./components/Main";
+import NavBar from "./components/Navabr";
+
+export default function App() {
+    return (
+        <div className="container">
+            <Main />
+            <NavBar />
+        </div>
+    )
+}
---
+++ ProjectSetUpPractice/components/Navabr.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <h1>Navbar goes here</h1>
+    )
+}
