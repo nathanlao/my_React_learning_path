0eb66e2f1cc8fcaa6067556d06e963a892be2335
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-28 21:33:52 -0800

    Fixed typo

--- my-react-learning-app/src/components/MainContent.js
+++ my-react-learning-app/src/components/MainContent.js
@@ -9,12 +9,12 @@ export default function MainContent() {
             <ul className="content-list">
                 <li>What is composability?</li>
                 <span>The ability to have the components as a single tag, several React components can be combined to 
-                    produce another React component.then render it inside ReactDOM.render().</span>
+                    produce another React component, then render it inside ReactDOM.render().</span>
                 <li>Why is React declarative?</li>
                 <span>No need to tell the program step by step how should it be done.</span>
                 <li>What is JSX?</li>
                 <span>JavaScript XML (a flavor of JS that looks like HTML)
-                    makes React declarative. It allow us to write HTML elements in JS and put in in the DOM
+                    makes React declarative. It allow us to write HTML elements in JS and put it in the DOM
                     without any createElement() methods.</span>
                 <li>How to set up React?</li>
                 <span>For jsx syntax to work, we need to import the following:</span>
