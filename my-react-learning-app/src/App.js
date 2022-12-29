31f13bee64193efb01b53c01c5a846a39fc41545
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-28 19:52:30 -0800

    Style the main content

--- my-react-learning-app/src/App.js
+++ my-react-learning-app/src/App.js
@@ -5,7 +5,7 @@ import MainContent from "./components/MainContent"
 
 export default function App() {
     return (
-        <div className="main">
+        <div>
             <Header />
             <MainContent />
             <Footer />
--- my-react-learning-app/src/components/MainContent.js
+++ my-react-learning-app/src/components/MainContent.js
@@ -4,15 +4,14 @@ import React from "react"
 export default function MainContent() {
     return (
         // Children components
-        <div>
-            <h1>My React learning path</h1>
-            <h3 className="content-subheader">My first day learning React xD</h3>
+        <main>
+            <h1 className="content-title">My first day learning React xD</h1>
             <ol className="content-list">
                 <li>What is composability?</li>
                 <li>Why is React declarative?</li>
                 <li>What is JSX?</li>
                 <li>How to set up React?</li>
             </ol>
-        </div>
+        </main>
     )
 }
