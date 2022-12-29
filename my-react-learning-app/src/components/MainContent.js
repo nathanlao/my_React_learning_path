9bda81db000206307b1d87b587b98675c4c3c194
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-28 20:06:30 -0800

    Style the bullet point

--- my-react-learning-app/src/components/MainContent.js
+++ my-react-learning-app/src/components/MainContent.js
@@ -6,12 +6,12 @@ export default function MainContent() {
         // Children components
         <main>
             <h1 className="content-title">My first day learning React xD</h1>
-            <ol className="content-list">
+            <ul className="content-list">
                 <li>What is composability?</li>
                 <li>Why is React declarative?</li>
                 <li>What is JSX?</li>
                 <li>How to set up React?</li>
-            </ol>
+            </ul>
         </main>
     )
 }
