68095520d1503c455c75efe753652685ee31f290
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-29 22:24:38 -0800

    Pass in non string props

--- PropsPractice/App.js
+++ PropsPractice/App.js
@@ -7,10 +7,22 @@ export default function App() {
             <Joke 
                 setup="I got my daughter a fridge for her birthday."
                 punchline="I can't wait to see her face light up when she opens it."
+                // pass in non-string props by wrapping with {} --> give us the free reign
+                // to throw in any JS
+                isPun={false} 
+                upvotes={10}
+                downvotes={2}
+                comments={[{author:"", body:"", title:""}]}
             />
             <Joke 
                 setup="How did the hacker escape the police?"
                 punchline="He just ransomware!"
+                isPun={true}
+            />
+            <Joke 
+                setup="test"
+                punchline="test"
+                isPun={true}
             />
         </div>
     )
