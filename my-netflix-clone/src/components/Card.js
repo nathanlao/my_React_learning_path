6fb3a537ff371715e9aacad2ecb9fc5811052ae5
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-29 20:59:32 -0800

    Edited typo

--- my-netflix-clone/src/components/Card.js
+++ my-netflix-clone/src/components/Card.js
@@ -9,7 +9,7 @@ export default function Card() {
             <p><span className="bold">Deadpool</span> February 12</p>
             <div className="card--stats">
                 <img alt="star" src={star} className="card--star"/>
-                <span className="gray">2016 • </span>
+                <span className="gray">7.5</span>
                 <span className="gray">Action • </span>
                 <span className="gray">1h 48m</span>
             </div>
