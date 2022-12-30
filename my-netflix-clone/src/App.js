986fc0ad76d5e3dc2ab185d41cb10a4ca99df7cc
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-30 02:41:52 -0800

    map data into card components

--- my-netflix-clone/src/App.js
+++ my-netflix-clone/src/App.js
@@ -2,21 +2,36 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import cardData from "./data"
 
 export default function App() {
+    // map data into card components -> return a new array with card components using props
+    const cardElements = cardData.map((card) => {
+        return <Card 
+                img={card.img}
+                title={card.title}
+                date={card.date}
+                rating={card.rating}
+                type={card.type}
+                timeLen={card.timeLen}
+                />
+    })
+
     return (
         <div>
             <Navbar />
             {/* <Hero /> */}
             {/* pass props to Card*/}
-            <Card 
+            {/* <Card 
                 img="deadpool.png"
                 title="Deadpool"
                 date="February 12"
                 rating={7.5}
                 type="Action"
                 timeLen="1h 48m"
-            />
+            /> */}
+
+            {cardElements}
         </div>
     )
 }
---
+++ my-netflix-clone/src/data.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        img: "deadpool.png",
+        title: "Deadpool",
+        date: "February 12",
+        rating: 7.5,
+        type: "Action",
+        timeLen: "1h 48m",
+    },
+    {
+        img: "avatar.png",
+        title: "Avatar",
+        date: "December 16",
+        rating: 8.0,
+        type: "Sci-fi",
+        timeLen: "3h 12m",
+    },
+    {
+        img: "babylon.png",
+        title: "Babylon",
+        date: "December 23",
+        rating: 6.5,
+        type: "Drama",
+        timeLen: "3h 9m",
+    },
+    {
+        img: "knivesout.png",
+        title: "Knives Out",
+        date: "November 27",
+        rating: 7.0,
+        type: "Crime",
+        timeLen: "2h 10m", 
+    }
+]
