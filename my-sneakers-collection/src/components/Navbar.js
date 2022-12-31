cf73f808d5d91bbd4a69e98082ac86da6932a419
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-30 17:13:40 -0800

    style the navbar

--- my-sneakers-collection/src/components/Navbar.js
+++ my-sneakers-collection/src/components/Navbar.js
@@ -3,9 +3,9 @@ import sneakerLogo from "../images/sneaker-logo.png"
 
 export default function Navbar() {
     return (
-        <nav>
-            <img alt="logo" src={sneakerLogo} />
-            <h1>my sneaker collection</h1>
+        <nav className="nav">
+            <img alt="logo" src={sneakerLogo} className="nav--logo" />
+            <h1 className="nav--title">my sneaker collection</h1>
         </nav>
     )
 }
