906566859172a52743217eb958665710750099a5
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-30 18:12:31 -0800

    Style the card component

--- my-sneakers-collection/src/components/Card.js
+++ my-sneakers-collection/src/components/Card.js
@@ -4,15 +4,26 @@ import character from "../images/character.png"
 
 export default function Card() {
     return (
-        <div>
-            <img alt="jordon" src={jordonOne} />
-            <div>
-            <img alt="character" src={character} />
-                <h3>USA</h3>
+        <div className="card">
+            <div className="card--img-wrapper">
+                <img alt="jordon" src={jordonOne} className="card--image"/>
             </div>
-            <h1>Air Jordon 1</h1>
-            <h3>December 12</h3>
-            <p>This is intro</p>
+            <div className="card--info">
+                <p className="card--info--location">
+                    <span>
+                        <img className="card--info--logo" alt="character" src={character} />
+                    </span>
+                    USA {" "}
+                    <a
+                      href="https://www.flightclub.com/the-10-air-jordan-1-white-black-varsity-red-black-801781"
+                      className="card--info--link">  
+                    View on Flight Club</a>
+                </p>
+                <h2 className="card--info--title">Air Jordon 1</h2>
+                <p className="card--info--date">Release Date: December 12</p>
+                <p className="card--info--desc">The OFF-WHITE x Air Jordan 1 is created by designer Virgil Abloh in the classic Chicago colorway. 
+                Some of the half-finished characteristics of the sneaker include an OFF-WHITE zip tie on the collar, a Swoosh linked to the shoe with noticeable blue stitching, 'Air' printed on the midsole. The box is deconstructed as it is turned inward with the inside consisting of a black structure and a gold Jumpman logo, while the outside is plain cardboard with the words 'Jumpman' printed on it.</p>
+            </div>
         </div>
     )
 }
