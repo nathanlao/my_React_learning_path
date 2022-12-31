4d273a6f3c0992b99f02e635ec68ba07c8385a0f
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-30 19:31:29 -0800

    map card component with object

--- my-sneakers-collection/src/App.js
+++ my-sneakers-collection/src/App.js
@@ -1,21 +1,18 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Card from "./components/Card"
+import sneakerData from "./data"
 
 export default function App() {
+    const sneakerElement = sneakerData.map((sneaker) => {
+        return <Card 
+            sneaker={sneaker}/> 
+    })
+
     return (
         <div>
             <Navbar />
-            <Card 
-                img="airjordon-one.png"
-                brand="Air Jordon"
-                link="https://www.flightclub.com/the-10-air-jordan-1-white-black-varsity-red-black-801781"
-                title="Air Jordon 1"
-                date="December 12"
-                desc="The OFF-WHITE x Air Jordan 1 is created by designer Virgil Abloh in the classic Chicago colorway. 
-                Some of the half-finished characteristics of the sneaker include an OFF-WHITE zip tie on the collar, a Swoosh linked to the shoe with noticeable blue stitching, 'Air' printed on the midsole. 
-                The box is deconstructed as it is turned inward with the inside consisting of a black structure and a gold Jumpman logo, while the outside is plain cardboard with the words 'Jumpman' printed on it."
-                />
+            {sneakerElement}
         </div>
     )
 }
--- my-sneakers-collection/src/components/Card.js
+++ my-sneakers-collection/src/components/Card.js
@@ -2,24 +2,25 @@ import React from "react";
 import jordonOne from "../images/airjordon-one.png"
 import character from "../images/character.png"
 
+
 export default function Card(props) {
     return (
         <div className="card">
             <div className="card--img-wrapper">
-                <img alt="jordon" src={require(`../images/${props.img}`)} className="card--image"/>
+                <img alt="jordon" src={require(`../images/${props.sneaker.img}`)} className="card--image"/>
             </div>
             <div className="card--info">
                 <p className="card--info--location">
                     <span>
                         <img className="card--info--logo" alt="character" src={character} />
                     </span>
-                    {props.brand} {" "}
-                    <a href={props.link} className="card--info--link">  
+                    {props.sneaker.brand} {" "}
+                    <a href={props.sneaker.link} className="card--info--link">  
                     View on Flight Club</a>
                 </p>
-                <h2 className="card--info--title">{props.title}</h2>
-                <p className="card--info--date">Release Date: {props.date}</p>
-                <p className="card--info--desc">{props.desc}</p>
+                <h2 className="card--info--title">{props.sneaker.title}</h2>
+                <p className="card--info--date">Release Date: {props.sneaker.date}</p>
+                <p className="card--info--desc">{props.sneaker.desc}</p>
             </div>
         </div>
     )
