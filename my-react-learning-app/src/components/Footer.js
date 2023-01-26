50489b0a9e8f99522370c322f39d925e9e22bc29
nathanlao <gla93@sfu.ca>
2023-01-25 21:24:01 -0800

    use useState() to flip the color mode use props to pass the state to child component to update conditionally render the dark mode

--- my-react-learning-app/src/components/Footer.js
+++ my-react-learning-app/src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-export default function Footer() {
+export default function Footer(props) {
     return (
-        <footer className="footer">
+        <footer className={props.darkMode ? "dark" : ""}>
             <small>@ 2022 Nathan developement. All rights reserved.</small>
         </footer>
     )
