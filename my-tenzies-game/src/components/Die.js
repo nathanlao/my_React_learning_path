e6363457271ae313a3256fc1bfcd8210dbcb9982
nathanlao <gla93@sfu.ca>
2023-01-29 02:06:19 -0800

    useEffect() to keep track of won game

--- my-tenzies-game/src/components/Die.js
+++ my-tenzies-game/src/components/Die.js
@@ -7,7 +7,7 @@ export default function Die(props) {
     }
 
     return (
-        <div className="die-face" style={styles}>
+        <div className="die-face" style={styles} onClick={props.handleHoldDice}>
             <h2 className="die-number">{props.value}</h2>
         </div>
     )
