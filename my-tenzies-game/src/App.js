88dcdb769528541d5b33b20cd7b0e22f95407bfc
nathanlao <gla93@sfu.ca>
2023-01-28 23:53:00 -0800

    Generate random numbers in array and map over the array to die component

--- my-tenzies-game/src/App.js
+++ my-tenzies-game/src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Die from "./components/Die"
 
 export default function App() {
+
+    function allNewDice() {
+        const newDiceArray = []
+        for (let i = 0; i < 10; i++) {
+            // random number from 1 - 6
+            const randomNumber = Math.floor(Math.random() * 6) + 1
+            newDiceArray.push(randomNumber)
+        }
+        return newDiceArray
+    }
+
+    // State to hold the array of numbers
+    const [numbers, setNumbers] = useState(allNewDice())
+
+    // Map over the numbers array -> array of die element with value props
+    const diceElements = numbers.map((number, index) => {
+        return <Die key={index} value={number} />
+    })
+
     return (
         <main>
-            <h1>hello world</h1>
+            <div className="dice-container">
+               {diceElements}
+            </div>
         </main>
     )
 }
