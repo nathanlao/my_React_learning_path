d046d986479ec4e22638e5fc09886fcb47773ab8
nathanlao <gla93@sfu.ca>
2023-02-27 19:53:56 -0800

    Using Link to navigate to diff routes

--- 02.ReactRouter6/my-van-life-app/src/index.js
+++ 02.ReactRouter6/my-van-life-app/src/index.js
@@ -2,20 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // 1. BrowserRouter under the hood is a context provider 
 //    that provide context to all its children (Routes component)
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
+    return (
+        // 2. Wrap the entire App with BrowserRouter to 
+        //    enable client routing
+        <BrowserRouter>
+            <Link to="/">Home</Link>
+            <Link to="about">About</Link>
+            <Routes>
+                {/* Path to the route && which react element to render */}
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+function Home() {
     return (
         <h1>Hello world!</h1>
     )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    // 2. Wrap the entire App with BrowserRouter to enable client routing
-    <BrowserRouter>
-        <Routes>
+function About() {
+    return (
+        <h1>About page!</h1>
+    )
+}
 
-        </Routes>
-        <App />
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App/>
 )
