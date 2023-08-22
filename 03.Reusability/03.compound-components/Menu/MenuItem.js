96b906f8765a545f511b088bd3f4f6f6f199956f
nathanlao <gla93@sfu.ca>
2023-08-22 16:06:48 -0700

    Flatten MenuDropdown

---
+++ 03.Reusability/03.compound-components/Menu/MenuItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function MenuItem({children}) {
+    return (
+        <div className="menu-item">
+            {children}
+        </div>
+    )    
+}
--- 03.Reusability/03.compound-components/index.js
+++ 03.Reusability/03.compound-components/index.js
@@ -3,22 +3,36 @@ import ReactDOM from 'react-dom/client';
 import Menu from "./Menu/Menu"
 import MenuButton from "./Menu/MenuButton"
 import MenuDropdown from "./Menu/MenuDropdown"
+import MenuItem from "./Menu/MenuItem"
 
 /**
+ * Flatten the structure by using compound components:
+ * 
  * 1. Have the Menu component to use props.children 
  * 
  * 2. Render MenuButton and MenuDropdown as children of the Menu component
  * 
  * 3. Have the MenuButton to accept props.children and render
+ * 
+ * 4. Have the MenuDropdown to render children
+ * 
+ * 5. MenuItem also renders children
+ * 
+ * 6. As an "user", you now take contol of how you want MenuItem to be rendered.
+ * loop over the items in index.js
  */
 
 function App() {
+
+    const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
     return (
         <Menu>
             <MenuButton>Sports</MenuButton>
-            <MenuDropdown 
-                items={["Tennis", "Pickleball", "Racquetball", "Squash"]}
-            />
+            <MenuDropdown>
+                {sports.map((sport) => (
+                    <MenuItem key={sport}>{sport}</MenuItem>
+                ))}
+            </MenuDropdown>
         </Menu>
     )
 }
