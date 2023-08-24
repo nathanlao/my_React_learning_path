7dcf4dfb8597f97f87f443bd2dac58697ffa2cf2
nathanlao <gla93@sfu.ca>
2023-08-24 15:41:43 -0700

    Provide ThemeContext for Button component

--- 03.Reusability/04.context/Button.js
+++ 03.Reusability/04.context/Button.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { ThemeContext } from './App'
 
 export default function Button() {
+    // Provide the correct context to React.useContext()
+    const value = React.useContext(ThemeContext);
+
     return (
-        <button className="dark-theme">
+        <button className={`${value}-theme`}>
             Switch Theme
         </button>
     )
