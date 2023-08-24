4ad67bbb1739974b817ddd54e693ad68f6b335d5
nathanlao <gla93@sfu.ca>
2023-08-24 15:29:40 -0700

    Header used the ThemeContext

--- 03.Reusability/04.context/Header.js
+++ 03.Reusability/04.context/Header.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { ThemeContext } from "./App"
 
 export default function Header() {
+    // Provide the correct context to React.useContext()
+    const value = React.useContext(ThemeContext)
+
     return (
-        <header className="dark-theme">
-            <h1>Dark Theme</h1>
+        <header className={`${value}-theme`}>
+            <h1>{value === 'light' ? 'Light' : 'Dark'} Theme</h1>
         </header>
     )
 }
