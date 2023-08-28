d6ecd4005fbd0cc2210459884024597b3a9f339c
nathanlao <gla93@sfu.ca>
2023-08-27 20:57:27 -0700

    refractor Menu: handle the logic using Toggle

--- 03.Reusability/05.headless-components/index.js
+++ 03.Reusability/05.headless-components/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Star from "./Star"
 import Toggle from "./components/Toggle/index"
+import Menu from "./components/Menu/index"
+
 import { BsStar, BsStarFill } from "react-icons/bs"
 
 function App() {
+
+    const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
+
     return (
         <>
             {/* 1. Setup <Toggle> as parent, use context to 
@@ -29,6 +34,24 @@ function App() {
                         <BsStar className="star"/>
                     </Toggle.Off>
                 </Toggle.Button>
+            </Toggle>
+
+            {/* 2. In this example: Menu components are refractored
+            to use the logic from Toggle instead of doing any logic 
+            of its own. */}
+            <Toggle>
+                <Menu>
+                    <Toggle.Button>
+                        <Menu.Button>Sports</Menu.Button>
+                    </Toggle.Button>
+                    <Toggle.On>
+                        <Menu.Dropdown>
+                            {sports.map((sport) => (
+                                <Menu.Item key={sport}>{sport}</Menu.Item>
+                            ))}
+                        </Menu.Dropdown>
+                    </Toggle.On>
+                </Menu>
             </Toggle>
         </>
     )
