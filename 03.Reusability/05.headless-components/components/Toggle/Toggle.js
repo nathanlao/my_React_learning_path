97817685a75b5252067b55f138c42796ca788c0b
nathanlao <gla93@sfu.ca>
2023-09-01 21:50:40 -0700

    ontoggle use ref to keep track of the first render

--- 03.Reusability/05.headless-components/components/Toggle/Toggle.js
+++ 03.Reusability/05.headless-components/components/Toggle/Toggle.js
@@ -5,13 +5,21 @@ const ToggleContext = React.createContext();
 export default function Toggle({ children, onToggle }) {
 
     const [on, setOn] = React.useState(false);
+    
+    // Only want to run onToggle() AFTER the first time 
+    // rendering the component
+    const firstRender = React.useRef(true);
 
     function toggle() {
         setOn(prevOpen => !prevOpen)
     }
 
     React.useEffect(() => { 
-        onToggle();
+        if (firstRender.current) {
+            firstRender.current = false;
+        } else { 
+            onToggle();
+        }
     }, [on])
 
     return (
