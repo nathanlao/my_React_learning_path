7b0146e31d7d5e59b57ccf19f9ad0c0d2f386ee2
nathanlao <gla93@sfu.ca>
2023-09-01 21:58:45 -0700

    fix missing onToggle bug using noop fun

--- 03.Reusability/05.headless-components/components/Toggle/Toggle.js
+++ 03.Reusability/05.headless-components/components/Toggle/Toggle.js
@@ -2,10 +2,18 @@ import React from "react";
 
 const ToggleContext = React.createContext();
 
-export default function Toggle({ children, onToggle }) {
+/**
+ * noop function: 
+ * onToggle = () => {}
+ * Default it to an empty function if no onToggle() is passed to <Toggle />
+ * 
+ * Avoid the issue if onToggle is not passed to Toggle component
+ * and onToggle is undefined
+ *  */ 
+export default function Toggle({ children, onToggle = () => {}}) {
 
     const [on, setOn] = React.useState(false);
-    
+
     // Only want to run onToggle() AFTER the first time 
     // rendering the component
     const firstRender = React.useRef(true);
