3db93464752b912434f0e56317c72035e748ccfc
nathanlao <gla93@sfu.ca>
2023-09-03 17:23:01 -0700

    useToggle to simplified Star comp

--- 03.Reusability/09.custom-hooks/components/Star.js
+++ 03.Reusability/09.custom-hooks/components/Star.js
@@ -1,18 +1,27 @@
 import React from "react"
 import Toggle from "./Toggle/index"
 import { BsStar, BsStarFill } from "react-icons/bs"
+import useToggle from "../hook/useToggle"
 
 export default function Star({ onChange }) {
+    // use the useToggle hook to conditionally render the stars
+    const [on, toggle] = useToggle();
+
     return (
-        <Toggle onToggle={onChange}>
-            <Toggle.Button>
-                <Toggle.On>
-                    <BsStarFill className="star filled" />
-                </Toggle.On>
-                <Toggle.Off>
-                    <BsStar className="star" />
-                </Toggle.Off>
-            </Toggle.Button>
-        </Toggle>
+        
+        on ? <BsStarFill onClick={toggle} className="star filled" />
+            : <BsStar onClick={toggle} className="star" />
+
+
+        // <Toggle onToggle={onChange}>
+        //     <Toggle.Button>
+        //         <Toggle.On>
+        //             <BsStarFill className="star filled" />
+        //         </Toggle.On>
+        //         <Toggle.Off>
+        //             <BsStar className="star" />
+        //         </Toggle.Off>
+        //     </Toggle.Button>
+        // </Toggle>
     )
 }
--- 03.Reusability/09.custom-hooks/index.js
+++ 03.Reusability/09.custom-hooks/index.js
@@ -7,18 +7,21 @@ import Toggle from "./components/Toggle/index"
 function App() {
   return (
     <>
-      <Toggle onToggle={() => {console.log("toggled")}}>
-        <Toggle.Button>
-          <Toggle.Display>
-            {(on) => {
-              return <div className={`box ${on ? "filled" : ""}`}></div>
-            }}
-          </Toggle.Display>
-        </Toggle.Button>
-      </Toggle>
+      <Star />
     </>
   )
 }
+// Example using Toggle.Display for the render props
+      // <Toggle onToggle={() => {console.log("Toggled")}}>
+      //   <Toggle.Button>
+      //     <Toggle.Display>
+      //       {(on) => {
+      //         return <div className={`box ${on ? "filled" : ""}`}></div>
+      //       }}
+      //     </Toggle.Display>
+      //   </Toggle.Button>
+      // </Toggle>
+
 // Example using Toggle.On and Toggle.Off to conditionally render items
           // <Toggle.On>
           //   <div className="box filled"></div>
